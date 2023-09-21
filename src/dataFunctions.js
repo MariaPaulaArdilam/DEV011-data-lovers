be70e25bd6123dd4c270ea9fbae4b96a99b5b45e
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-09-21 17:05:12 -0500

    arreglo sintaxis html

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,7 +1,10 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => {
+  console.log("data", data);
+  console.log("filterBy", filterBy);
+  console.log("value", value);
+  return 1;
 };
 
 export const anotherExample = () => {
