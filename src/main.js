53388972a62fdc97d883a383a0a02584814c1a1e
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-09-25 11:31:16 -0500

    se hizo la funcion del filtro

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import { example } from './dataFunctions.js';
 import { renderItems } from "./view.js";
 import { filterData } from "./dataFunctions.js";
+import {sortData} from "./dataFunctions.js";
 
 // import data from './data/lol/lol.js';
 import data from "/data/ghibli/ghibli.js";
@@ -11,10 +12,15 @@ console.log(filterData, renderItems, data);
 const root = document.getElementById("root");
 root.appendChild(renderItems(data.films));
 
-const datafilm = data;
+const datafilm = data.films;
 const selectFilter = document.getElementsByName("Filter")[0];
 
 selectFilter.addEventListener("change", () => {
   const resulFilter = filterData(datafilm, "director", selectFilter.value);
   console.log(resulFilter);
+  root.innerHTML = ''
+  root.appendChild(renderItems(resulFilter))
 });
+
+const dataorden = data.films;
+const sortData = document.getElementsByName("alphabet")
