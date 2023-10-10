04b6de4514cc33594c0dadbe31e44d21baad887f
MarcelaGaravito <mache095@gmail.com>
2023-10-10 07:37:25 -0600

    main

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import { sortData } from "./dataFunctions.js";
 import { computeStats } from "./dataFunctions.js";
 import data from "./data/ghibli/ghibli.js";
 
-console.log(computeStats, renderItems, data);
-
-//filtro Director
 const root = document.getElementById("root");
 root.appendChild(renderItems(data.films));
 
@@ -20,7 +17,7 @@ selectFilter.addEventListener("change", () => {
   root.innerHTML = "";
   root.appendChild(renderItems(resulFilter));
 });
-//Ordenar por alfabeto
+
 const selectSort = document.getElementsByName("alphabet")[0];
 
 selectSort.addEventListener("change", () => {
@@ -36,7 +33,7 @@ selectSort.addEventListener("change", () => {
   // console.log(resultsort);
 });
 
-//Estadistica según Score
+///calculo
 const estadistica = document.querySelector(".top-5");
 estadistica.addEventListener("dblclick", () => {
   const peliculasScoreMayor95 = computeStats(data);
@@ -57,10 +54,12 @@ estadistica.addEventListener("click", () => {
 });
 
 //Función Boton limpiar.
+
 const ResetBotton = document.querySelector(
   "button[data-testid='button-clear']"
 );
-ResetBotton.addEventListener("click", () => {
+ResetBotton.addEventListener("click", (event) => {
+  const clickedButton = event.target;
   root.innerHTML = "";
   estadistica.innerHTML = "Datos";
   root.appendChild(renderItems(data.films));
